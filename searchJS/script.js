6834b19983fa62d45154a8fcc8e0f14dfdedb735
nhanvi2308 <nhan.vi@gleads.vn>
2021-07-31 23:03:01 +0700

    add search js

---
+++ searchJS/script.js
@@ -0,0 +1,35 @@
+// const input = document.getElementById("search");
+// input.addEventListener("keyup", search);
+
+// function search() {
+//     const inputVal = input.value;
+//     const li = document.getElementsByTagName("li");
+
+//     for (i = 0; i < li.length; i++) {
+//         if (li[i].innerHTML.toLowerCase().includes(inputVal)) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+const input = document.getElementById('search');
+input.addEventListener('keyup', search);
+
+
+function search() {
+    const inputVal = input.value;
+    const li = document.getElementsByTagName('li');
+
+    console.log(li);
+    for (let index = 0; index < li.length; index++) {
+        if (li[index].innerHTML.toLowerCase().includes(inputVal)) {
+            li[index].style.display = "";
+        } else {
+            li[index].style.display = "none";
+        }
+
+
+    }
+}
