0e9a42e80c2b17b549dd61ccf0e265c3e67b058a
nhanvi2308 <nhan.vi@gleads.vn>
2021-08-08 21:38:34 +0700

    Sticky Nav Bar on Scroll

---
+++ Sticky Nav Bar on Scroll/script.js
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+
+// Sticky Header 
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header'); // target Header not class header
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
