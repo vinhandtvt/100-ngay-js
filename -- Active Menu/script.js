38a2f8e6bc38d4078db9208bb7478693b30a4b18
nhanvi2308 <nhan.vi@gleads.vn>
2021-08-08 22:05:57 +0700

    Active menu switcher

---
+++ -- Active Menu/script.js
@@ -0,0 +1,23 @@
+// // Active Menu Switcher
+
+// const navList = document.querySelector(".nav-list");
+
+// navList.addEventListener("click", (e) => {
+//   const navLink = e.target.parentElement;
+//   if (navLink.classList.contains("link")) {
+//     navList.querySelector(".active").classList.remove("active");
+//     navLink.classList.add("active");
+//   }
+// });
+
+// Active Menu Switcher
+
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener('click', (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains('link')) {
+    navList.querySelector(".active").classList.remove('active');
+    navLink.classList.add('active')
+  }
+})
